a24722497ef1c7537bd55b6a5359584105ab24b3
Mariusz <mariusz@mariusz-desktop.(none)>
2010-07-27 17:52:50 +0200

    filter helper

--- app/helpers/search_alternative_helpers.rb
+++ app/helpers/search_alternative_helpers.rb
@@ -1,40 +1,75 @@
 module SearchAlternativeHelpers
   def order(search, options = params[:search], html_options = {}, page = params[:page], page_params = params)
 
-    options[:params_scope] ||= :search
-    if !options[:as]
-      id = options[:by].to_s.downcase == "id"
-      options[:as] = id ? options[:by].to_s.upcase : options[:by].to_s.humanize
-    end
-    options[:ascend_scope] ||= "ascend_by_#{options[:by]}"
-    options[:descend_scope] ||= "descend_by_#{options[:by]}"
-    ascending = search.ordering.to_s == options[:ascend_scope]
-    new_scope = ascending ? options[:descend_scope] : options[:ascend_scope]
-    selected = [options[:ascend_scope], options[:descend_scope]].include?(search.ordering.to_s)
-    if selected
-      css_classes = html_options[:class] ? html_options[:class].split(" ") : []
-      if ascending
-        options[:as] = raw "&#9650;&nbsp;#{options[:as]}"
-        css_classes << "ascending"
-      else
-        options[:as] = raw "&#9660;&nbsp;#{options[:as]}"
-        css_classes << "descending"
+    url_options = {
+                          :search => {}
+    }
+
+    if options && options[:by]
+      options[:params_scope] ||= :search
+      if !options[:as]
+        id = options[:by].to_s.downcase == "id"
+        options[:as] = id ? options[:by].to_s.upcase : options[:by].to_s.humanize
       end
-      html_options[:class] = css_classes.join(" ")
+      options[:ascend_scope] ||= "ascend_by_#{options[:by]}"
+      options[:descend_scope] ||= "descend_by_#{options[:by]}"
+      ascending = search.ordering.to_s == options[:ascend_scope]
+      new_scope = ascending ? options[:descend_scope] : options[:ascend_scope]
+      selected = [options[:ascend_scope], options[:descend_scope]].include?(search.ordering.to_s)
+      if selected
+        css_classes = html_options[:class] ? html_options[:class].split(" ") : []
+        if ascending
+          options[:as] = raw "&#9650;&nbsp;#{options[:as]}"
+          css_classes << "ascending"
+        else
+          options[:as] = raw "&#9660;&nbsp;#{options[:as]}"
+          css_classes << "descending"
+        end
+        html_options[:class] = css_classes.join(" ")
+      end
+      #url_options = {
+      #        options[:params_scope] => search.conditions.merge({:ordering => new_scope})
+      #}.deep_merge(options[:params] || {})
+
+      url_options =
+              {
+                      options[:params_scope] => {:ordering => new_scope}
+              }
+
     end
-    #url_options = {
-    #        options[:params_scope] => search.conditions.merge({:ordering => new_scope})
-    #}.deep_merge(options[:params] || {})
-    url_options =
-            {
-                    options[:params_scope] => {:ordering => new_scope}
-            }
+
+    if options && options[:by_scope]
+      url_options[:search][:by_scope] = options[:by_scope]
+
+
+      if page_params[:search] && page_params[:search][:by_scope] == options[:by_scope]
+
+        css_classes = html_options[:class] ? html_options[:class].split(" ") : []
+        if page_params[:search] && page_params[:search][:scope_direction] == "ascending"
+          options[:as] = raw "&#9660;&nbsp;#{options[:as]}"
+          css_classes << "descending"
+        else
+          options[:as] = raw "&#9650;&nbsp;#{options[:as]}"
+          css_classes << "ascending"
+        end
+        html_options[:class] = css_classes.join(" ")
+      end
+
+      if page_params[:search] && page_params[:search][:scope_direction] == "ascending"
+        url_options[:search][:scope_direction] = "descending"
+
+      else
+        url_options[:search][:scope_direction] = "ascending"
+
+      end
+    end
+
     if page
       url_options[:page] = page
     end
     if page_params[:search]
       if page_params[:search][:with]
-        url_options[:search][:with] = page_params[:search][:with] 
+        url_options[:search][:with] = page_params[:search][:with]
       end
       if page_params[:search][:role_name]
         url_options[:search][:role_name] = page_params[:search][:role_name]
@@ -42,9 +77,52 @@ module SearchAlternativeHelpers
       if page_params[:search][:role_object]
         url_options[:search][:role_object] = page_params[:search][:role_object]
       end
+      if page_params[:search][:filter]
+        url_options[:search][:filter] = page_params[:search][:filter]
+      end
+      if page_params[:search][:filter_params]
+        url_options[:search][:filter_params] = page_params[:search][:filter_params]
+      end
+#      if page_params[:search][:by_scope]
+#        url_options[:search][:by_scope] = page_params[:search][:by_scope]
+#      end
+#      if page_params[:search][:scope_direction]
+#        url_options[:search][:scope_direction] = page_params[:search][:scope_direction]
+#      end
     end
 
     options[:as] = raw(options[:as]) if defined?(RailsXss)
+
+    link_to options[:as], url_for(url_options), html_options
+  end
+
+  def filter(filter_scope, options = params[:search], html_options = {}, page_params = params)
+    url_options = {
+                          :search => {}
+    }
+    if filter_scope
+      url_options[:search][:filter] = filter_scope
+      if options[:filter_params]
+        url_options[:search][:filter_params] = options[:filter_params]
+      end
+    end
+    if page_params[:search]
+      if page_params[:search][:with]
+        url_options[:search][:with] = page_params[:search][:with]
+      end
+      if page_params[:search][:role_name]
+        url_options[:search][:role_name] = page_params[:search][:role_name]
+      end
+      if page_params[:search][:role_object]
+        url_options[:search][:role_object] = page_params[:search][:role_object]
+      end
+      if page_params[:search][:by_scope]
+        url_options[:search][:by_scope] = page_params[:search][:by_scope]
+      end
+      if page_params[:search][:scope_direction]
+        url_options[:search][:scope_direction] = page_params[:search][:scope_direction]
+      end
+    end
 
     link_to options[:as], url_for(url_options), html_options
   end
--- lib/search_methods.rb
+++ lib/search_methods.rb
@@ -1,12 +1,32 @@
 module SearchMethods
   def search(params = {})
-    search_ordering= params && params[:search] ? params[:search][:ordering] : nil
-    @search_ordering = search_ordering.to_s
-    search_ordering = /^asc/.match(search_ordering) ? 'ASC' : 'DESC'
+    if params && params[:search]
+      search_ordering= params && params[:search] ? params[:search][:ordering] : nil
+      @search_ordering = search_ordering.to_s
+      search_ordering = /^asc/.match(search_ordering) ? 'ASC' : 'DESC'
+      @search_by = params[:search] ? params[:search][:with] : nil
+      #search_by = params && params[:search] ? params[:search][:by] : nil
 
-    @search_by = params[:search] ? params[:search][:with] : nil
-    #search_by = params && params[:search] ? params[:search][:by] : nil
-    build_where(@search_by).build_order(search_ordering, ordering_by(@search_ordering))
+      build_where(@search_by).build_order(search_ordering, ordering_by(@search_ordering)).use_search_scope(params[:search]).use_search_filter_scope(params[:search])
+    else
+      where("true")
+    end
+  end
+
+  def use_search_scope(search_params)
+    if search_params[:by_scope] && search_params[:scope_direction]
+      send("#{search_params[:scope_direction]}_by_#{search_params[:by_scope]}")
+    else
+      where("true")
+    end
+  end
+
+  def use_search_filter_scope(search)
+    if search[:filter]
+      send("filter_by_#{search[:filter]}".to_sym, search[:filter_params] ? search[:filter_params] : nil)
+    else
+      where("true")
+    end
   end
 
   def build_order(search_ordering, search_by)
@@ -20,7 +40,7 @@ module SearchMethods
   def build_where(search_by)
     result = 'TRUE'
     if search_by
-      search_by.each do |key,value|
+      search_by.each do |key, value|
         result += " and #{key} like '%#{value}%'"
       end
     end
